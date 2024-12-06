4df1480d1f9dc1879a8817d08f66bdbf19aacad0
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-06 18:21:10 -0300

    feat: A função create_tables() é responsável por garantir que a tabela movies seja criada no banco de dados

--- app.py
+++ app.py
@@ -17,3 +17,26 @@ def initialize_connection():
 
 def get_connection():
     return global_connection
+
+
+# A função create_tables() é responsável por garantir que a tabela movies seja criada no banco de dados
+def create_tables():
+    print("Inicializando o banco de dados...")
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        # Criar tabela se não existir
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS movies (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            year INTEGER,
+            title TEXT,
+            studios TEXT,
+            producers TEXT,
+            winner BOOLEAN
+        )
+        """)
+        conn.commit()
+        print("Tabela 'movies' criada ou já existe.")
+    except Exception as e:
+        print(f"Erro ao criar tabelas: {e}")
